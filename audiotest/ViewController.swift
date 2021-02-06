476c61b73b78170ffde45e53883d03eb928bf97f
Tung <donly@magicalboy.com>
2021-02-06 08:01:34 +0800

    Initial Commit

---
+++ audiotest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  audiotest
+//
+//  Created by Tung Lam Chan on 6/2/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
